441c81de6eb202497c77353e426d4c828cd567f5
adamnebylitsa <adamnebylitsa@gmail.com>
2024-03-02 15:14:00 -0500

    Tutle can move

--- webpage/app.py
+++ webpage/app.py
@@ -16,22 +16,42 @@ class FlaskPublisher(Node):
 
     def __init__(self):
         super().__init__('FlaskPublisher')
-        self.publisher_ = self.create_publisher(Twist, 'topic', 10)
+        self.publisher_ = self.create_publisher(Twist, 'turtle1/cmd_vel', 10)
         timer_period = 0.5  # seconds
+        self.msg=Twist()
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
-        msg = Twist()
-        self.publisher_.publish(msg)
-        print(msg)
+        self.publisher_.publish(self.msg)
+
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/buttons')
+@app.route('/buttons', methods=['GET', 'POST'])
 def buttons():
+    if 'forward' in request.form:
+        flask_publisher.msg.linear.x=1.0
+        flask_publisher.msg.angular.z=0.0
+        flask_publisher.publisher_.publish(flask_publisher.msg)
+    elif 'backward' in request.form:
+        flask_publisher.msg.linear.x=-1.0
+        flask_publisher.msg.angular.z=0.0
+        flask_publisher.publisher_.publish(flask_publisher.msg)
+    elif 'right' in request.form:
+        flask_publisher.msg.linear.x=1.0
+        flask_publisher.msg.angular.z=-0.5
+        flask_publisher.publisher_.publish(flask_publisher.msg)
+    elif 'left' in request.form:
+        flask_publisher.msg.linear.x=1.0
+        flask_publisher.msg.angular.z=0.5
+        flask_publisher.publisher_.publish(flask_publisher.msg)
+    elif 'stop' in request.form:
+        flask_publisher.msg.linear.x=0.0
+        flask_publisher.msg.angular.z=0.0
+        flask_publisher.publisher_.publish(flask_publisher.msg)
     return render_template('buttons.html')
 
 @app.route('/signal')
@@ -44,20 +64,21 @@ def path():
 
 
 def main(args=None):
-    rclpy.init(args=args)
+    pass
 
-    flask_publisher= FlaskPublisher()
-
-    rclpy.spin(flask_publisher)
+    #rclpy.spin(flask_publisher)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    flask_publisher.destroy_node()
-    rclpy.shutdown()
+    #flask_publisher.destroy_node()
+    #rclpy.shutdown()
 
 
 if __name__ == '__main__':
+    rclpy.init(args=None)
+    flask_publisher=FlaskPublisher()
     app.run(debug=True)
-    main()
+    flask_publisher.destroy_node()
+    rclpy.shutdown()
 
